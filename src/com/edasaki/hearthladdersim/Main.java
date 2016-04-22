b57bb0ea25aed89e28bf77b9ee9b82307819393f
Edasaki <x@ronbo.me>
2016-04-22 16:21:00 -0500

    initial commit

---
+++ src/com/edasaki/hearthladdersim/Main.java
@@ -0,0 +1,138 @@
+package com.edasaki.hearthladdersim;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Main {
+
+    /**
+     * index X = stars needed to reach rank X 
+     */
+    private static final int[] STAR_REQS = {
+            5, //0
+            5, //1
+            5, //2
+            5, //3
+            5, //4
+            5, //5
+            5, //6
+            5, //7
+            5, //8
+            5, //9
+            4, //10
+            4, //11
+            4, //12
+            4, //13
+            4, //14
+            3, //15
+            3, //16
+            3, //17
+            3, //18
+            3, //19
+    };
+
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        final int testCount = 1000000;
+        ArrayList<String> output = new ArrayList<String>();
+        output.add("");
+        for (Strategy s : Strategy.values()) {
+            output.addAll(simulate(testCount, s));
+            output.add("");
+        }
+        for (String s : output)
+            System.out.println(s);
+    }
+
+    public static ArrayList<String> simulate(int testCount, Strategy strategy) {
+        System.out.println("Beginning simulation of " + strategy.toString() + " - " + strategy.getDescription());
+        double[] statsArr = new double[6]; //count, time, longestWinStreak, longestLoseStreak};
+        int statusUpdate = (int) (testCount * 0.05);
+        for (int k = 0; k < testCount; k++) {
+            int[] temp = simulateClimb(strategy, false);
+            for (int j = 0; j < temp.length; j++)
+                statsArr[j] += temp[j];
+            if ((k + 1) % statusUpdate == 0)
+                System.out.println("Testing run " + (k + 1));
+        }
+        for (int k = 0; k < statsArr.length; k++) {
+            statsArr[k] = statsArr[k] / testCount;
+        }
+        int hours = (int) (statsArr[1]) / 60;
+        int minutes = (int) (statsArr[1]) % 60;
+        ArrayList<String> output = new ArrayList<String>();
+        output.add("Simulated " + testCount + " Rank 20 -> Legend runs.");
+        output.add("Strategy: " + strategy.toString() + " - " + strategy.getDescription());
+        output.add("Averages: " + statsArr[0] + " games. " + hours + " hours & " + minutes + " minutes.");
+        output.add("Average longest  win streak: " + statsArr[2]);
+        output.add("Average longest lose streak: " + statsArr[3]);
+        output.add("Average # games at rank 5 or better: " + statsArr[4]);
+        output.add("Average # games at ranks worse than 5: " + statsArr[5]);
+        output.add("Average % of games at rank 5 or better: " + (statsArr[4] / statsArr[0]));
+        return output;
+    }
+
+    public static int[] simulateClimb(Strategy s, boolean verbose) {
+        int streak = 0;
+        int stars = 0;
+        int count = 0;
+        int rank = 20;
+        int time = 0;
+        int longestWinStreak = 0;
+        int longestLoseStreak = 0;
+        int superProCount = 0;
+        int noobCount = 0;
+        while (rank > 0) {
+            boolean win = false;
+            time += s.getTime(streak, rank);
+            if (rank > 5) {
+                noobCount++;
+            } else {
+                superProCount++;
+            }
+            if (Math.random() < s.getRate(streak, rank)) {
+                win = true;
+                if (streak < 0)
+                    streak = 0;
+                streak++;
+                stars++;
+                if (streak >= 3 && rank > 5)
+                    stars++;
+            } else {
+                if (streak > 0)
+                    streak = 0;
+                streak--;
+
+                stars--;
+            }
+            if (stars < 0) {
+                if (rank == 20) {
+                    stars = 0;
+                } else {
+                    stars = STAR_REQS[rank] - 1;
+                    rank++;
+                }
+            } else if (stars > STAR_REQS[rank - 1]) {
+                stars -= STAR_REQS[rank - 1];
+                rank--;
+            }
+            if (verbose)
+                System.out.println((win ? " Won" : "Lost") + " game, now at rank " + rank + " with " + stars + " stars, streak " + streak);
+            if (streak > longestWinStreak)
+                longestWinStreak = streak;
+            if (streak < longestLoseStreak)
+                longestLoseStreak = streak;
+            count++;
+        }
+        int hours = time / 60;
+        int minutes = time % 60;
+        if (verbose) {
+            System.out.println("Games: " + count + ", Time: " + time + " minutes = " + hours + " hours & " + minutes + " minutes");
+            System.out.println("Longest  win streak: " + longestWinStreak);
+            System.out.println("Longest lose streak: " + longestLoseStreak);
+            System.out.println("Games at rank 5 or better: " + superProCount);
+            System.out.println("Games at ranks worse than 5: " + noobCount);
+        }
+        return new int[] { count, time, longestWinStreak, longestLoseStreak, superProCount, noobCount };
+    }
+
+}
---
+++ src/com/edasaki/hearthladdersim/Strategy.java
@@ -0,0 +1,73 @@
+package com.edasaki.hearthladdersim;
+
+public enum Strategy {
+    FAST(0.52, 5, "52% win rate with 5 minute matches. Aggro face decks. Face Hunter, Face Paladin"),
+    MEDIUM(0.55, 7, "55% win rate with 7 minute matches. Mid-range decks. Examples: Midrange Druid, Secret Paladin"),
+    BAD_SLOW(0.55, 10, "55% win rate with 10 minute matches. Slow control decks. Win-rate equivalent to MEDIUM decks. Examples: Control Warrior, Dragon Priest"),
+    SLOW(0.62, 10, "62% win rate with 10 minute matches. Slow control decks. Played well enough to maintain a high win rate. Examples: Control Warrior, Dragon Priest"),
+    MY_PATRON(0.58, 7, "My own 58% win rate on Patron (19-14) with an average of 7 minute matches."),
+    MY_FLOOD_PALLY(0.71, 7, "My own 71% win rate on Flood Pally (30-12) with an average of 6 minute matches."),
+    MY_FACE_HUNTER(0.57, 5, "My own 57% win rate on Face Hunter (41-31) with an average of 5 minute matches."),
+    MY_MURLOC_PALLY(0.47, 8, "My own 47% win rate on Anyfin Paladin (26-29) with an average of 8 minute matches."),
+    MY_MILL_ROGUE(0.50, 9, "My own 50% win rate on Mill Rogue (19-19) with an average of 9 minute matches."),
+    SWITCH_STRATEGY_1(0, 0, "Play FAST decks until 2 wins in a row, then play SLOW to attempt to get a longer winstreak. Won't ever play SLOW in legend."),
+    SWITCH_STRATEGY_2(0, 0, "Play FAST decks until 2 wins in a row, then play BAD_SLOW to attempt to get a longer winstreak. Won't ever play BAD_SLOW in legend."),
+    SWITCH_STRATEGY_3(0, 0, "Play SLOW decks until 2 wins in a row, then play FAST to attempt to get a longer winstreak. Won't ever play FAST in legend."),
+
+    CARD_MASTER(0.95, 5, "95% win rate with 5 minutes matches. Pls teach me your ways.");
+    private double rate;
+    private int time;
+    private String desc;
+
+    public double getTime(int streak, int rank) {
+        if (this == Strategy.SWITCH_STRATEGY_1) {
+            if (streak > 2 && rank > 5)
+                return SLOW.time;
+            else
+                return FAST.time;
+        } else if (this == Strategy.SWITCH_STRATEGY_2) {
+            if (streak > 2 && rank > 5)
+                return BAD_SLOW.time;
+            else
+                return FAST.time;
+        } else if (this == Strategy.SWITCH_STRATEGY_3) {
+            if (streak > 2 && rank > 5)
+                return FAST.time;
+            else
+                return SLOW.time;
+        } else {
+            return time;
+        }
+    }
+
+    public double getRate(int streak, int rank) {
+        if (this == Strategy.SWITCH_STRATEGY_1) {
+            if (streak > 2 && rank > 5)
+                return SLOW.rate;
+            else
+                return FAST.rate;
+        } else if (this == Strategy.SWITCH_STRATEGY_2) {
+            if (streak > 2 && rank > 5)
+                return BAD_SLOW.rate;
+            else
+                return FAST.rate;
+        } else if (this == Strategy.SWITCH_STRATEGY_3) {
+            if (streak > 2 && rank > 5)
+                return FAST.rate;
+            else
+                return SLOW.rate;
+        } else {
+            return rate;
+        }
+    }
+
+    public String getDescription() {
+        return desc;
+    }
+
+    Strategy(double rate, int time, String description) {
+        this.rate = rate;
+        this.time = time;
+        this.desc = description;
+    }
+}
